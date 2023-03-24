5e853230da4170bf76b5c1d800d3c4f05129ca4e
Leonardo Cáceres <benjamonben@yahoo.com.ar>
2023-03-23 23:20:29 -0300

    creacion API

---
+++ servidor.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+
+
+app.listen(3001, () => console.log("Escuchando el servidor"));
+
+app.get('/puto',(req, res)=> {
+    res.json({mensaje:'Ines Diaz Alegria'}).status(200)
+});
+
