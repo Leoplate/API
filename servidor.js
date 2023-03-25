db2a12c0c2c61cb3429781058281b57aef1ffff2
Leonardo Cáceres <benjamonben@yahoo.com.ar>
2023-03-24 23:13:13 -0300

    Generacion servidor y app3

--- servidor.js
+++ servidor.js
@@ -5,8 +5,10 @@ const app = express();
 
 app.listen(3001, () => console.log("Escuchando el servidor"));
 
-app.get('/puto',(req, res)=> {
-    res.json({mensaje:'Ines Diaz Alegria'}).status(200)
+app.get('/cliente',(req, res)=> {
+    res.json({nombre:'Ines Diaz Alegria',
+              direccion: 'Forly 787',
+              dni:'92608198'}).status(200)
 });
   
   
